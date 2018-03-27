844b017adb7e8c8dae3d0e6e6b757fd7f0f2c52c
zirriga <zirriga@mail.ru>
2018-03-27 20:59:23 +0300

    update: get parent, get children (2 methods) WorkInProgress: tests

--- src/binar.java
+++ src/binar.java
@@ -1,9 +1,13 @@
+import jdk.nashorn.internal.runtime.regexp.joni.ast.AnyCharNode;
+import org.omg.CORBA.Any;
+
+import java.util.ArrayList;
 
 public class binar<t1 extends Comparable<t1>, t2> {
     static class node<t1, t2> {
         t1 key;
         t2 value;
-        node<t1, t2> left, right;
+        node<t1, t2> left, right, parent;
 
         node(t1 key, t2 value) {
             this.key = key;
@@ -117,5 +121,54 @@ public class binar<t1 extends Comparable<t1>, t2> {
             x.key = leftMost.key;
             x.value = leftMost.value;
         }
-    } //+ получение родителя и потомков допиасать
+    }
+    public t2 getParent(t1 k) {
+        node<t1, t2> x = root, y = null;
+        while ((x.right != null)|(x.left != null)) {
+            int cmp = k.compareTo(x.key);
+            if (cmp == 0) {
+                return y.value;
+            }
+            if (cmp < 0) {
+                x = x.left;
+                y = x;
+            } else {
+                x = x.right;
+                y = x;
+            }
+        }
+        return null;
+    }
+    public t2 getRightChild(t1 k) {
+        node<t1, t2> x = root;
+        while (x != null) {
+            int cmp = k.compareTo(x.key);
+            if (cmp == 0) {
+                x = x.right;
+                return x.value;
+            }
+            if (cmp < 0) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        return null;
+    }
+    public t2 getLeftChild(t1 k) {
+        node<t1, t2> x = root;
+        while (x != null) {
+            int cmp = k.compareTo(x.key);
+            if (cmp == 0) {
+                x = x.left;
+                return x.value;
+            }
+            if (cmp < 0) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        return null;
+    }
 }
